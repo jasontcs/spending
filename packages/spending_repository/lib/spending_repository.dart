a0b9f64f044dc6d023fb5ed9ac80e48056d6f0de
Jason Tse <jason_cs_tse@vtech.com>
2022-07-28 18:13:58 +0800

    repo interface

--- packages/spending_repository/lib/spending_repository.dart
+++ packages/spending_repository/lib/spending_repository.dart
@@ -1,7 +1,3 @@
-library spending_repository;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/model/model.dart';
+export 'src/spending_repository.dart';
+export 'src/spending_repository_impl.dart';
