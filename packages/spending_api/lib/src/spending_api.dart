fd8d335ecd81aa0bd979a7089ac48fcdc0afafaf
Jason Tse <jason_cs_tse@vtech.com>
2022-07-29 17:24:05 +0800

    whatever

--- packages/spending_api/lib/src/spending_api.dart
+++ packages/spending_api/lib/src/spending_api.dart
@@ -1,36 +1,53 @@
+import 'dart:io';
+
 import '../spending_api.dart';
 
 abstract class SpendingApi {
-  Stream<List<ApiCategoryModel>> getCategories({
-    String? id,
+  Stream<List<ApiCategoryModel>> get categoriesStream;
+  Stream<List<ApiCurrencyModel>> get currenciesStream;
+  Stream<List<ApiPersonModel>> get peopleStream;
+  Stream<List<ApiReceiptModel>> get receiptsStream;
+  Stream<List<ApiRecordModel>> get recordsStream;
+
+  Future<List<ApiCategoryModel>> getCategories({
+    String? recordId,
+    String? title,
   });
-  Stream<List<ApiCurrencyModel>> getCurrencies({
-    String? id,
+  Future<List<ApiCurrencyModel>> getCurrencies({
+    String? recordId,
+    String? title,
   });
-  Stream<List<ApiPersonModel>> getPeople({
-    String? id,
-  });
-  Stream<List<ApiReceiptModel>> getReceipts({
-    String? id,
+  Future<List<ApiPersonModel>> getPeople({
+    String? title,
     String? recordId,
   });
-  Stream<List<ApiRecordModel>> getRecords({
-    String? id,
+  Future<List<ApiReceiptModel>> getReceipts({
+    String? recordId,
+    String? imageUrl,
+  });
+  Future<List<ApiRecordModel>> getRecords({
     String? categoryId,
     String? personId,
     DateTime? from,
     DateTime? to,
   });
 
-  Stream<ApiCategoryModel> getCategory(String id);
-  Stream<ApiCurrencyModel> getCurrency(String id);
-  Stream<ApiPersonModel> getPerson(String id);
-  Stream<ApiReceiptModel> getReceipt(String id);
-  Stream<ApiRecordModel> getRecord(String id);
+  Future<ApiCategoryModel> getCategory(String id);
+  Future<ApiCurrencyModel> getCurrency(String id);
+  Future<ApiPersonModel> getPerson(String id);
+  Future<ApiReceiptModel> getReceipt(String id);
+  Future<ApiRecordModel> getRecord(String id);
 
   Future<String?> editCategory(String? id, ApiCategoryModel? category);
   Future<String?> editCurrency(String? id, ApiCurrencyModel? currency);
   Future<String?> editPerson(String? id, ApiPersonModel? person);
   Future<String?> editReceipt(String? id, ApiReceiptModel? receipt);
   Future<String?> editRecord(String? id, ApiRecordModel? record);
+
+  Future<String?> uploadImage({
+    required File file,
+    required String id,
+    required String type,
+  });
+  Future<String?> downloadImage(String path);
 }
--- packages/spending_api/test/spending_api_test.dart
+++ packages/spending_api/test/spending_api_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:spending_api/src/firestore_spending_api.dart';
+import 'package:spending_api/src/firebase_spending_api.dart';
 
 void main() {
   test('adds one to input values', () {
