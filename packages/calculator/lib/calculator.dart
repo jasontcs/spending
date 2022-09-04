b9afee5ce19c1e7ecf72e44b013b7f26b028af6d
jasontcs <10681380+jasontcs@users.noreply.github.com>
2022-09-04 08:08:40 +0800

    calculator

---
+++ packages/calculator/lib/calculator.dart
@@ -0,0 +1,223 @@
+library calculator;
+
+import 'dart:async';
+
+import 'package:decimal/decimal.dart';
+
+enum CalculatorOperator {
+  add('+'),
+  minus('-'),
+  multiply('×'),
+  divide('÷');
+
+  final String value;
+
+  const CalculatorOperator(this.value);
+}
+
+enum CalculatorNumber {
+  one(1),
+  two(2),
+  three(3),
+  four(4),
+  five(5),
+  six(6),
+  seven(7),
+  eight(8),
+  nine(9),
+  zero(0);
+
+  final int value;
+
+  const CalculatorNumber(this.value);
+
+  String get string => value.toString();
+}
+
+class CalculatorValue {
+  final String? first;
+  final CalculatorOperator? operator;
+  final String? second;
+
+  const CalculatorValue({
+    this.first,
+    this.operator,
+    this.second,
+  });
+
+  CalculatorValue copyWith({
+    String? first,
+    CalculatorOperator? operator,
+    String? second,
+  }) =>
+      CalculatorValue(
+        first: first ?? this.first,
+        operator: operator ?? this.operator,
+        second: second ?? this.second,
+      );
+}
+
+class CalcualtorNotInit implements Exception {}
+
+class CalcualtorInputNotValid implements Exception {}
+
+class Calculator {
+  CalculatorValue? _value;
+  final _controller = StreamController<CalculatorValue>();
+  late final Stream<CalculatorValue> snapshot =
+      _controller.stream.asBroadcastStream();
+  CalculatorValue get value =>
+      _value != null ? _value! : throw CalcualtorNotInit();
+
+  void _update(CalculatorValue value) {
+    _controller.add(value);
+    _value = value;
+  }
+
+  void init({num? initialValue}) {
+    _update(CalculatorValue(first: initialValue?.toString()));
+  }
+
+  void input(CalculatorNumber number) {
+    if (value.second != null) {
+      _update(value.copyWith(second: value.second! + number.string));
+    } else if (value.operator != null) {
+      _update(value.copyWith(second: number.string));
+    } else if (value.first != null) {
+      _update(value.copyWith(first: value.first! + number.string));
+    } else {
+      _update(value.copyWith(first: number.string));
+    }
+  }
+
+  void dot() {
+    if (value.second != null) {
+      if (value.second!.contains('.')) {
+        _controller.addError(CalcualtorInputNotValid());
+      } else {
+        _update(value.copyWith(second: '${value.second!}.'));
+      }
+    } else if (value.operator != null) {
+      _update(value.copyWith(second: '0.'));
+    } else if (value.first != null) {
+      if (value.first!.contains('.')) {
+        _controller.addError(CalcualtorInputNotValid());
+      } else {
+        _update(value.copyWith(first: '${value.first!}.'));
+      }
+    } else {
+      _update(value.copyWith(first: '0.'));
+    }
+  }
+
+  void remove() {
+    if (value.second != null) {
+      final second = _removeLastChar(value.second!);
+      _update(CalculatorValue(
+        first: value.first,
+        operator: value.operator,
+        second: second,
+      ));
+    } else if (value.operator != null) {
+      _update(CalculatorValue(
+        first: value.first,
+        second: value.second,
+      ));
+    } else if (value.first != null) {
+      final first = _removeLastChar(value.first!);
+      _update(CalculatorValue(
+        first: first,
+        operator: value.operator,
+        second: value.second,
+      ));
+    }
+  }
+
+  String? _removeLastChar(String string) {
+    if (string.length <= 1) return null;
+    return string.substring(0, string.length - 1);
+  }
+
+  void clear() {
+    _update(const CalculatorValue());
+  }
+
+  void operate(CalculatorOperator operator) {
+    if (value.second != null) {
+      final second = value.second!.endsWith('.')
+          ? _removeLastChar(value.second!)!
+          : value.second!;
+      final result = _calculateString(value.first!, value.operator!, second);
+      _update(CalculatorValue(
+        first: result,
+        operator: operator,
+        second: null,
+      ));
+    } else if (value.operator != null) {
+      _update(value.copyWith(operator: operator));
+    } else if (value.first != null) {
+      final first = value.first!.endsWith('.')
+          ? _removeLastChar(value.first!)
+          : value.first!;
+      _update(value.copyWith(first: first, operator: operator));
+    } else {
+      _controller.addError(CalcualtorInputNotValid());
+    }
+  }
+
+  String _calculateString(
+      String first, CalculatorOperator operator, String second) {
+    switch (operator) {
+      case CalculatorOperator.add:
+        return (Decimal.parse(first) + Decimal.parse(second))
+            .round(scale: 2)
+            .toString();
+      case CalculatorOperator.minus:
+        return (Decimal.parse(first) - Decimal.parse(second))
+            .round(scale: 2)
+            .toString();
+      case CalculatorOperator.multiply:
+        return (Decimal.parse(first) * Decimal.parse(second))
+            .round(scale: 2)
+            .toString();
+      case CalculatorOperator.divide:
+        return (Decimal.parse(first) / Decimal.parse(second))
+            .toDecimal(scaleOnInfinitePrecision: 2)
+            .round(scale: 2)
+            .toString();
+    }
+  }
+
+  void calculate() {
+    if (value.second == null || value.operator == null || value.first == null) {
+      _controller.addError(CalcualtorInputNotValid());
+    } else {
+      final result = _calculateString(
+        value.first!,
+        value.operator!,
+        value.second!,
+      );
+      _update(
+        CalculatorValue(
+          first: result,
+          operator: null,
+          second: null,
+        ),
+      );
+    }
+  }
+
+  void done() {
+    if (value.second != null && value.operator != null && value.first != null) {
+      calculate();
+    } else {
+      _update(
+        CalculatorValue(
+          first: value.first,
+          operator: null,
+          second: null,
+        ),
+      );
+    }
+  }
+}
---
+++ packages/calculator/test/calculator_test.dart
@@ -0,0 +1,89 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:calculator/calculator.dart';
+
+void main() {
+  void expectCalculator(
+    CalculatorValue value,
+    String? first,
+    CalculatorOperator? operator,
+    String? second,
+  ) {
+    expect(value.first, first);
+    expect(value.operator, operator);
+    expect(value.second, second);
+  }
+
+  test('1+1', () {
+    final calculator = Calculator()
+      ..init()
+      ..input(CalculatorNumber.one)
+      ..operate(CalculatorOperator.add)
+      ..input(CalculatorNumber.one);
+    expectCalculator(calculator.value, '1', CalculatorOperator.add, '1');
+  });
+  test('1+1=', () {
+    final calculator = Calculator()
+      ..init()
+      ..input(CalculatorNumber.one)
+      ..operate(CalculatorOperator.add)
+      ..input(CalculatorNumber.one)
+      ..calculate();
+    expectCalculator(calculator.value, '2', null, null);
+  });
+
+  test('.1', () {
+    final calculator = Calculator()
+      ..init()
+      ..dot()
+      ..input(CalculatorNumber.one);
+    expectCalculator(calculator.value, '0.1', null, null);
+  });
+
+  test('.1<', () {
+    final calculator = Calculator()
+      ..init()
+      ..dot()
+      ..input(CalculatorNumber.one)
+      ..remove();
+    expectCalculator(calculator.value, '0.', null, null);
+  });
+  test('.13', () {
+    final calculator = Calculator()
+      ..init()
+      ..dot()
+      ..input(CalculatorNumber.one)
+      ..input(CalculatorNumber.three);
+
+    expectCalculator(calculator.value, '0.13', null, null);
+  });
+  test('.13<2++-.1x', () {
+    final calculator = Calculator()
+      ..init()
+      ..dot()
+      ..input(CalculatorNumber.one)
+      ..input(CalculatorNumber.three)
+      ..remove()
+      ..input(CalculatorNumber.two)
+      ..operate(CalculatorOperator.add)
+      ..operate(CalculatorOperator.add)
+      ..operate(CalculatorOperator.minus)
+      ..dot()
+      ..input(CalculatorNumber.one)
+      ..operate(CalculatorOperator.multiply);
+
+    expectCalculator(
+        calculator.value, '0.02', CalculatorOperator.multiply, null);
+  });
+
+  test('1/7', () {
+    final calculator = Calculator()
+      ..init()
+      ..input(CalculatorNumber.one)
+      ..operate(CalculatorOperator.divide)
+      ..input(CalculatorNumber.seven)
+      ..calculate();
+
+    expectCalculator(calculator.value, '0.14', null, null);
+  });
+}
