c93fc58f1d10fb468d97a6d170810c2dd46b2b41
jasontcs <10681380+jasontcs@users.noreply.github.com>
2022-09-04 08:08:56 +0800

    remove orginal

--- packages/spending_repository/lib/src/spending_repository.dart
+++ packages/spending_repository/lib/src/spending_repository.dart
@@ -2,12 +2,6 @@ import 'dart:io';
 
 import '../spending_repository.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-
 abstract class SpendingRepository {
   Stream<List<Record>> get recordsStream;
   Stream<List<Category>> get categoriesStream;
